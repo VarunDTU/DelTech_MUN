17b7161e7f3ec36b15679dbe98b35e1d3ac43ef5
Varun <varun.py10@gmail.com>
2022-11-05 12:59:47 +0530

    Revert "added sticky navbar with onhover-colors"
    This reverts commit f057107339eda3f2595f3001930d03bc92f1fc3b.

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,21 +2,30 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header class="text-gray-600 body-font sticky top-0 backdrop-blur">
-      <div class="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
-        
+    <header class="text-gray-600 body-font">
+      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <img class="h-8 pr-1 mx-auto" src="/deltech-logo.png"></img>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
           <span class="ml-3 text-xl">DelTech MUN</span>
         </a>
         <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          <a class="mr-5 hover:text-red-600">HOME</a>
-          <a class="mr-5 hover:text-red-600">ABOUT</a>
-          <a class="mr-5 hover:text-red-600">SPEAKERS</a>
-          <a class="mr-5 hover:text-red-600">EVENTS</a>
-          <a class="mr-5 hover:text-red-600">CONTACT</a>
+          <a class="mr-5 hover:text-gray-900">First Link</a>
+          <a class="mr-5 hover:text-gray-900">Second Link</a>
+          <a class="mr-5 hover:text-gray-900">Contact</a>
+          <a class="mr-5 hover:text-gray-900">About Us</a>
         </nav>
-        <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-red-800 hover:text-white rounded text-base mt-4 md:mt-0">
+        <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
           Register Now
           <svg
             fill="none"
