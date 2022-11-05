ca784e2bcd69b508a076df3e00c43beffbd22b48
Vaibhav <vaibhav2438@gmail.com>
2022-11-05 13:08:02 +0530

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,31 +4,153 @@ import { FaLink } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="text-gray-600 px-24">
-        <div className="flex flex-col">
-          <h2 className="text-4xl flex flex-row text-gray-900 font-semibold mb-4 md:text-5xl">
-            <span>
-              <FaLink className="mr-5 text-blue-500" />
-            </span>
-            <span>Connect</span>
-          </h2>
-          <div className="text-xl font-medium space-x-8 flex flex-col ml-12 mb-24 space-y-4 sm:flex-row">
-            <p></p>
-            <p className="hover:text-red-600">
-              <Link href="/">Youtube</Link>
-            </p>
-            <p className="hover:text-blue-600">
-              <Link href="/">Twitter</Link>
-            </p>
-            <p className="hover:text-green-600">
-              <Link href="/">Facebook</Link>
-            </p>
-            <p className="hover:text-purple-600">
-              <Link href="/">LinkedIn</Link>
-            </p>
+    <footer className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+        <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span className="ml-3 text-xl">Tailblocks</span>
+          </a>
+          <p className="mt-2 text-sm text-gray-500">
+            Air plant banjo lyft occupy retro adaptogen indego
+          </p>
+        </div>
+        <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              CATEGORIES
+            </h2>
+            <nav className="list-none mb-10">
+              <li>
+                <a className="text-gray-600 hover:text-gray-800">First Link</a>
+              </li>
+              <li>
+                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+              </li>
+              <li>
+                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+              </li>
+              <li>
+                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+              </li>
+            </nav>
+          </div>
+          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+              CATEGORIES
+            </h2>
+            <nav className="list-none mb-10">
+              <li>
+                <a className="text-gray-600 hover:text-gray-800">First Link</a>
+              </li>
+              <li>
+                <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+              </li>
+              <li>
+                <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+              </li>
+              <li>
+                <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+              </li>
+            </nav>
+          </div>
+          
+          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+           
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3425.9157176142376!2d77.11396523433706!3d28.746144483964486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d012681da5cb3%3A0x52552bdb4fa8ac89!2sDTU%20Entrance%20Gate%2C%20Delhi%20Technological%20University%2C%20Shahbad%20Daulatpur%20Village%2C%20Rohini%2C%20Delhi%2C%20110042!5e0!3m2!1sen!2sin!4v1667633231276!5m2!1sen!2sin"
+              width="350"
+              height="200"
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
           </div>
         </div>
-      </footer>
+      </div>
+      <div className="bg-gray-100">
+        <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+          <p className="text-gray-500 text-sm text-center sm:text-left">
+            © 2020 Tailblocks —
+            <a
+              href="https://twitter.com/knyttneve"
+              rel="noopener noreferrer"
+              className="text-gray-600 ml-1"
+              target="_blank"
+            >
+              @knyttneve
+            </a>
+          </p>
+          <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            <a className="text-gray-500">
+              <svg
+                fill="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+              </svg>
+            </a>
+            <a className="ml-3 text-gray-500">
+              <svg
+                fill="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+              </svg>
+            </a>
+            <a className="ml-3 text-gray-500">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+              </svg>
+            </a>
+            <a className="ml-3 text-gray-500">
+              <svg
+                fill="currentColor"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="0"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="none"
+                  d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                ></path>
+                <circle cx="4" cy="4" r="2" stroke="none"></circle>
+              </svg>
+            </a>
+          </span>
+        </div>
+      </div>
+    </footer>
   );
 };
 
