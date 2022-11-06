36b2f0f6cfa01fc166ef97400fe530dba5b77ece
VarunDTU <91000991+VarunDTU@users.noreply.github.com>
2022-11-06 12:52:25 +0000

    Deploying to gh-pages from @ VarunDTU/DelTech_MUN@bd36519116db7e72b123863879d7d96b3c51bc0b 🚀

---
+++ _next/static/Za_Cy39HA1xt7uMksnxcW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/590-1494e4b4d8adb607.js","static/css/8c6f58ec6b2b46f0.css","static/chunks/pages/index-dc9dd72c77b29248.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
